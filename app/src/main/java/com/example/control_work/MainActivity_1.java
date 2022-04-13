7ffcb742b4d1dbac5f6df1a4e20f70ff6996d62c
Nik1406 <n.v.bulatov@mail.ru>
2022-04-13 22:23:51 +1000

    13.04.2022

---
+++ app/src/main/java/com/example/control_work/MainActivity_1.java
@@ -0,0 +1,14 @@
+package com.example.control_work;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity_1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
